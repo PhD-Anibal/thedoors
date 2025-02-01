1711735c69f824199e02f0a88450bf4e8e21f3f2
Anibal M. <anibal.maldonado@gmail.com>
2025-02-01 13:07:43 -0300

    alterando detalhes

--- interrupto.c
+++ interrupto.c
@@ -1,56 +1,47 @@
-// Anibal Maldonado: Código principal  - https://github.com/PhD-Anibal
-// Anibal Maldonado: LED Vermelho ao apertar 'A' - https://github.com/PhD-Anibal
-
+// Anibal Maldonado: - https://github.com/PhD-Anibal
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include <hardware/pio.h>           // Biblioteca para manipulação de periféricos PIO
-#include "hardware/clocks.h"        // Biblioteca para controle de relógios do hardware
+#include <hardware/pio.h>           // Biblioteca para controle dos periféricos PIO
+#include "hardware/clocks.h"        // Biblioteca para gerenciamento dos relógios do hardware
 #include "hardware/gpio.h"
-#include "Interrupto.pio.h" // Biblioteca PIO para controle de LEDs WS2818B
-#include "numeros.h"
-#include "hardware/timer.h" // Inclui a biblioteca para gerenciamento de temporizadores de hardware.
+#include "Interrupto.pio.h" // Biblioteca PIO para manipulação de LEDs WS2818B
+#include "numeros.h"        
+#include "hardware/timer.h" // Biblioteca para controle de temporizadores de hardware
 
+// Definições de constantes
+#define LED_COUNT 25                // Número total de LEDs na matriz
+#define LED_PIN 7                   // Pino GPIO utilizado para os LEDs
+#define DEBOUNCE_DELAY 100  // Tempo de debounce para evitar múltiplas detecções de um único toque
+volatile uint32_t last_interrupt_time=0; // Armazena o tempo da última interrupção
 
-const uint led_pin_red = 13; // porta do pino 13 LED RGB Vermelho
-const uint button_a = 5;
-const uint button_b = 6;
-static volatile uint a = 1 ;
+const uint led_pin_red = 13; // Pino do LED RGB (cor vermelha)
+const uint button_a = 5; // Pino do botão A
+const uint button_b = 6; // Pino do botão B
+static volatile uint a = 1 ; // Variável de controle
 static void gpio_irq_handler(uint gpio,uint32_t events);
 
-//para o state machine
+// Variáveis para o state machine
 PIO pio = pio0; 
 uint sm;
 bool ok;
 uint offset;
 
-//usei temporizador de hardware Anibal
-bool led_on = false;
-// Função de callback que será chamada repetidamente pelo temporizador
-// O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
-bool repeating_timer_callback(struct repeating_timer *t) {
-    // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
-    //printf("1 segundo passou\n");
-    //Liga ou desliga o led.
-    led_on = !led_on;
-    gpio_put(led_pin_red,led_on);
-    // Retorna true para manter o temporizador repetindo. Se retornar false, o temporizador para.
-    return true;
-}
-
-// Definições de constantes
-#define LED_COUNT 25                // Número de LEDs na matriz
-#define LED_PIN 7                   // Pino GPIO conectado aos LEDs
-#define FPS 10  // frames por segundo, indica que o LED vai piscar 10 vezes por segundo
-#define DEBOUNCE_DELAY 100  //tempo de debounce em milisegundos
-volatile uint32_t last_interrupt_time=0; //almaçenamos o tempo da ultima interrupção
-const int interval=1000/FPS; //intervalo de 100 ms
-
-// Anibal Maldonado - Índices da minha matriz de desenho na ordem desejada
+// Anibal Maldonado - Índices organizados da matriz para desenho na ordem desejada
 int ordem[] = {0,1, 2, 3, 4,9 , 8, 7,6,5, 10,11,12,13,14,19,18,17,16,15,20,21,22,23,24}; 
 int direcao=0;
 
+// Variável para controle do estado do LED
+bool led_on = false;
 
-//rotina para definição da intensidade de cores do led
+// Função de callback para o temporizador de hardware
+// Alterna o estado do LED vermelho a cada chamada
+bool repeating_timer_callback(struct repeating_timer *t) {
+    led_on = !led_on;
+    gpio_put(led_pin_red,led_on);
+    return true; // Mantém o temporizador ativo
+}
+
+// Função para definir a intensidade das cores do LED na matriz
 uint32_t matrix_rgb(double b, double r, double g)
 {
   unsigned char R, G, B;
@@ -60,67 +51,61 @@ uint32_t matrix_rgb(double b, double r, double g)
   return (G << 24) | (R << 16) | (B << 8);
 }
 
-//imprimir valor binário
-void imprimir_binario(int num) {
- int i;
- for (i = 31; i >= 0; i--) {
-  (num & (1 << i)) ? printf("1") : printf("0");
- }
-}
-
+// Função para desenhar um padrão de LED na matriz usando PIO
 void desenho_pio(int desenho[][25], uint32_t valor_led, PIO pio, uint sm, double r, double g, double b, int k) {
-    //for (int16_t k = 1; k < 10; k++) {
         for (int16_t i = 0; i < LED_COUNT; i++) {
                 switch (desenho[k][ordem[24-i]]) {
                     case 0: 
-                        valor_led = matrix_rgb(b=0.0, r=0.0, g=0.0);
+                        valor_led = matrix_rgb(b=0.0, r=0.0, g=0.0); // LED apagado
                         break;
                     case 1: 
-                        valor_led = matrix_rgb(b=1, r=0.0, g=0.0);
+                        valor_led = matrix_rgb(b=0.2, r=0.0, g=0.0); // LED azul aceso não muito intenso
                         break;
                 }
-
                 pio_sm_put_blocking(pio, sm, valor_led);
-
         }
-        imprimir_binario(valor_led);
 }
 
+// Função de interrupção para tratar eventos nos botões
 void gpio_irq_handler(uint gpio,uint32_t events)
 {
-    
     uint16_t i;
     uint32_t valor_led;
     double r = 0.0, b = 0.0 , g = 0.0;
     
-    //sleep_ms(50); // para o deboucing prevenindo a trepidação
-    uint32_t current_time=to_ms_since_boot(get_absolute_time()); //obtem o tempo atual em miliseguundos
+    uint32_t current_time=to_ms_since_boot(get_absolute_time()); // Obtém o tempo atual em milissegundos
     if(current_time-last_interrupt_time>DEBOUNCE_DELAY){
-        last_interrupt_time=current_time; //atualiza
+        last_interrupt_time=current_time; // Atualiza o tempo da última interrupção
         direcao=(gpio==button_a)?((direcao<9)?direcao+1:9):((direcao>0)?direcao-1:0);
         desenho_pio(desenho, valor_led, pio, sm, r, g, b,direcao);    
     }
-    
- 
 }
 
-int main() {
+void config() // Inicializa o serial, configura GPIOs, ativa PIO para os LEDs e define interrupções nos botões.
+{
     stdio_init_all(); // Inicializa a comunicação serial
 
-    gpio_init(led_pin_red); // Inicializa o pino do LED vermelho
-    gpio_set_dir(led_pin_red, GPIO_OUT); // Configura como saída
-    gpio_init(button_a); // Inicializa o pino do LED vermelho
-    gpio_set_dir(button_a, GPIO_IN); // Configura como saída
-    gpio_init(button_b); // Inicializa o pino do LED vermelho
-    gpio_set_dir(button_b, GPIO_IN); // Configura como saída
+    // Configuração do LED vermelho
+    gpio_init(led_pin_red);
+    gpio_set_dir(led_pin_red, GPIO_OUT);
+    
+    // Configuração dos botões
+    gpio_init(button_a);
+    gpio_set_dir(button_a, GPIO_IN);
+    gpio_init(button_b);
+    gpio_set_dir(button_b, GPIO_IN);
     gpio_pull_up(button_a);
     gpio_pull_up(button_b);
 
-   
-        //configurações da PIO
-    offset=offset = pio_add_program(pio, &interrupto_program);
+    // Configuração do periférico PIO
+    offset= pio_add_program(pio, &interrupto_program);
     sm = pio_claim_unused_sm(pio, true);
     interrupto_program_init(pio, sm, offset, LED_PIN);
+}
+
+int main() {
+    
+    config(); // função de configuração
 
     // Apaga todos os LEDs da matriz ao iniciar
     uint32_t valor_led = matrix_rgb(0.0, 0.0, 0.0);
@@ -128,18 +113,16 @@ int main() {
         pio_sm_put_blocking(pio, sm, valor_led);
     }
 
-
-    //stdio_init_all(); // Inicializa a comunicação serial (opcional)
+    // Configuração das interrupções nos botões
     gpio_set_irq_enabled_with_callback(button_a,GPIO_IRQ_EDGE_FALL, true,&gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(button_b,GPIO_IRQ_EDGE_FALL, true,&gpio_irq_handler);
-    
-    
+        
     struct repeating_timer timer;
-    // Configura o temporizador para chamar a função de callback a cada 0.1 segundo. 5 vezes por seg ligado e 5 desl
+    // Configura um temporizador para alternar o LED vermelho a cada 0.1 segundo
     add_repeating_timer_ms(100, repeating_timer_callback, NULL, &timer);
 
     while (true) {
-
+        // Loop infinito mantendo o programa em execução
     }
     
     return 0;
